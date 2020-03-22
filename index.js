673f8031a8d2dd9ab15841a7bf779a5674fa14dd
Mykhailo <mykhailo.hoy@gmail.com>
2020-03-22 22:37:56 +0200

    commit

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ $(function () {
 function createProjectCard(obj) {
 	var slides;
 	for (let e in obj.photos) {
-		slides += `<img src="img/projects/${obj.imgPath}/${obj.photos[e]}" loading="lazy">`
+		slides += `<img src="img/projects/${obj.imgPath}/${obj.photos[e]}">`
 	}
 
 	/* $.ajax({
